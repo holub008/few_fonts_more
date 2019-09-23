fb0040d7864d1307eaff83edb31f594529300b7e
Karl Holub <holub008@umn.edu>
2019-09-23 18:07:39 -0500

    focusing python on preprocessing

--- main.py
+++ main.py
@@ -1,51 +1,42 @@
 import cv2
 import numpy as np
-# heavily borrowed from http://cvisiondemy.com/extract-roi-from-image-with-python-and-opencv/
-
-import pytesseract
 
 #################
 # detect regions of interest (single characters) using simple binarization & contouring, since the white text is easy to
 # threshold on a relatively dark background.
+# also perform some preprocessing (borders + erosion) for tesseract
 #################
 
-credit_id = 1
-filename = "./images/%d.png" % (credit_id,)
-image = cv2.imread(filename)
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+n_images = 19
+for credit_id in range(1, n_images + 1):
+    filename = "./images/%d.png" % (credit_id,)
+    image = cv2.imread(filename)
+    grey_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    ret, thresh = cv2.threshold(grey_image, 127, 255, cv2.THRESH_BINARY)
 
-ret, thresh = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
-# TODO, even a cv2.waitKey results in cruddy behavior in pycharm
-# cv2.imshow('binarized_image', thresh)
+    ctrs, hier = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    sorted_ctrs = sorted(ctrs, key=lambda ctr: cv2.boundingRect(ctr)[0])
 
-kernel = np.ones((10, 1), np.uint8)
-img_dilation = cv2.dilate(thresh, kernel, iterations=1)
+    for i, ctr in enumerate(sorted_ctrs):
+        # getting the ROI
+        x, y, w, h = cv2.boundingRect(ctr)
+        binary_roi = thresh[y:y + h, x:x + w]
 
-ctrs, hier = cv2.findContours(img_dilation.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        border_size = 100
+        bordered_character = border = cv2.copyMakeBorder(
+            binary_roi,
+            top=border_size,
+            bottom=border_size,
+            left=border_size,
+            right=border_size,
+            borderType=cv2.BORDER_CONSTANT,
+            value=[0, 0, 0]
+        )
 
-sorted_ctrs = sorted(ctrs, key=lambda ctr: cv2.boundingRect(ctr)[0])
+        # since the font letters are so thick, erode to something the model likely understands better
+        kernel = np.ones((10, 10), np.uint8)
+        # tesseract is also extremely sensitive to background color, it seems, so we flip colors
+        eroded_image = cv2.bitwise_not(cv2.erode(bordered_character, kernel, iterations=1))
 
-for i, ctr in enumerate(sorted_ctrs):
-    # Get bounding box
-    x, y, w, h = cv2.boundingRect(ctr)
-
-    # Getting ROI
-    roi = image[y:y + h, x:x + w]
-    binary_roi = img_dilation[y:y + h, x:x + w]
-    cv2.imwrite("./images/raw_characters/%d_%d.jpg" % (credit_id, i), roi)
-
-    content = pytesseract.image_to_string(binary_roi, config="-l eng --psm 10")
-    print(content)
-
-    # this can provide a nice visual, when displaying all ROIs
-    # cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
-
-# cv2.imshow('all_rois', image)
-cv2.waitKey()
-
-
-##################
-# OCR the text in each character image
-##################
-
-# psm = page segmentation mode = 10 implies single character
+        cv2.imwrite("./images/extracted_characters/eroded_%d_%d.jpg" % (credit_id, i), eroded_image)
+        cv2.imwrite("./images/extracted_characters/binary_%d_%d.jpg" % (credit_id, i), binary_roi)
