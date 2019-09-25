31de2b511b0c72322bdfd9d5c3883a032eb7ae97
Karl Holub <holub008@umn.edu>
2019-09-25 00:30:12 -0500

    averaging for visualizing characters

--- ocr.R
+++ ocr.R
@@ -1,12 +1,13 @@
 library(tesseract)
 library(dplyr)
+library(imager)
 
 # note: it is possible to do this in python with pytesseract, but the API is horrible
 eng <- tesseract("eng", options = list(
   tessedit_char_whitelist="ABCDEFGHIJKLMNOPQRSTUVWXYZ "
 ))
 
-character_location <- "~/few_fonts_more/images/extracted_characters"
+character_location <- "/Users/kholub/few_fonts_more/images/extracted_characters"
 all_files <- list.files(character_location)
 
 certain_characters <- data.frame()
@@ -35,3 +36,26 @@ certain_characters %>%
 
 certain_characters %>%
   ggplot() + geom_histogram(aes(confidence))
+
+mean(certain_characters$confidence)
+
+certain_characters %>%
+  filter(word == 'J')
+
+plot_averaged_character <- function(characters, letter) {
+  characters %>%
+    filter(word == letter) %>%
+    pull(file) %>%
+    sapply(function(file) {
+      return(load.image(paste0(character_location, '/', file)))
+    }) %>%
+    lapply(function(img) {
+      return(resize(img, 200, 200))
+    }) %>%
+    average() %>%
+    plot()
+}
+
+for (letter in c('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'K', 'R', 'S', 'X', 'Z')) {
+  plot_averaged_character(certain_characters, letter)
+}
